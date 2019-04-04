17659f18a9efe9c8319e4058cd651bf959017025
Chenbo Yi <yichenbo@outlook.com>
2019-04-04 15:21:28 +0100

    Initialize @capcity with default value

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -2,10 +2,12 @@ require_relative 'bike'
 
 class DockingStation
   attr_reader :bike
+  attr_reader :capacity
   DEFAULT_CAPACITY = 20
 
-  def initialize
+  def initialize(capacity = DEFAULT_CAPACITY)
     @bikes = []
+    @capacity = capacity
   end
 
   def release_bike
@@ -21,7 +23,7 @@ class DockingStation
   private
 
   def full?
-    @bikes.count >= DEFAULT_CAPACITY
+    @bikes.count >= @capacity
   end
 
   def empty?
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -3,6 +3,10 @@ require 'DockingStation'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
+  it "has a DEFAULT_CAPACITY" do
+    expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+  end
+
   it 'releases a working bike' do
     bike = Bike.new
     expect(bike).to be_working
@@ -12,10 +16,17 @@ describe DockingStation do
     expect { subject.release_bike }.to raise_error("No bike is available")
   end
 
-  it "raise error if docking station is full" do
+  it "raise error if docking station is full on default capacity" do
     bike = Bike.new
-    DockingStation::DEFAULT_CAPACITY.times{subject.docking(bike)}
+    subject.capacity.times{subject.docking(bike)}
     expect { subject.docking(bike)}.to raise_error("Docking station full")
+  end
+
+  it "raise error if docking station is full on setted capacity" do
+    bike = Bike.new
+    docking_station = DockingStation.new(10)
+    10.times{docking_station.docking(bike)}
+    expect { docking_station.docking(bike)}.to raise_error("Docking station full")
   end
 
   it { is_expected.to respond_to(:docking).with(1).argument }
